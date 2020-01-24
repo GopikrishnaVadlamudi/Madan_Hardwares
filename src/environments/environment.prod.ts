f861af607c76fc2ef3e1a00b5d8b3d1ad0436ceb
unknown <gopi.vadlamudi10@gmail.com>
2020-01-25 01:43:29 +0530

    changes for new url madanhardwares.firebase.com

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,10 +2,10 @@ export const environment = {
   production: true,
   firebase: {
     apiKey: "AIzaSyCvHvw6tml2IpYz_P6J6bsZirAiWsgqBEU",
-    authDomain: "silken-alloy-87707.firebaseapp.com",
-    databaseURL: "https://silken-alloy-87707.firebaseio.com",
-    projectId: "silken-alloy-87707",
-    storageBucket: "silken-alloy-87707.appspot.com",
+    authDomain: "madanhardwares.firebaseapp.com",
+    databaseURL: "https://madanhardwares.firebaseio.com",
+    projectId: "madanhardwares",
+    storageBucket: "madanhardwares.appspot.com",
     messagingSenderId: "223112932940"    
   }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,11 +6,11 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "AIzaSyCvHvw6tml2IpYz_P6J6bsZirAiWsgqBEU",
-    authDomain: "silken-alloy-87707.firebaseapp.com",
-    databaseURL: "https://silken-alloy-87707.firebaseio.com",
-    projectId: "silken-alloy-87707",
-    storageBucket: "silken-alloy-87707.appspot.com",
+    apiKey: "AIzaSyDWGzQj2yJFSjJBlSAItTfkA_Ssenk6zH0",
+    authDomain: "madanhardwares.firebaseapp.com",
+    databaseURL: "https://madanhardwares.firebaseio.com",
+    projectId: "madanhardwares",
+    storageBucket: "madanhardwares.appspot.com",
     messagingSenderId: "223112932940"    
   }
 };
